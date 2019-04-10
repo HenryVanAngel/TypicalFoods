d0bcaa8c3e2f25ad9b3081b1d7f902b8d7a1d57f
HenryVanAngel <impossivel.errar@gmail.com>
2019-04-10 19:12:22 -0300

    Finalizado

---
+++ app/src/main/java/com/hiox/typicalfoods/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hiox.typicalfoods;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
